e21d5f6246c811daa46e95c775cb479aa45a7fc7
faheemalikhokhar <faheemali514@gmail.com>
2025-10-23 17:48:02 +0500

    lecture 5 added

---
+++ lecture_05.py
@@ -0,0 +1,50 @@
+# # if else statement in Python
+# num = int(input("Enter a number: "))
+# if num == 920:
+#     print("The number is 920")
+# elif num == 900:
+#     print("The number is 900")
+# else:
+#     print("The number is not 920")
+
+# # print("End of program")
+# total_price = int(input("Enter the total price: "))
+
+# if total_price <= 0:
+#     print("Invalid price")
+# elif total_price >= 2000:
+#      discount = total_price * 0.2
+#      final_price = total_price - discount
+#      print(f"Final price after discount: {final_price}")
+# elif total_price >= 3000:
+#      discount = total_price * 0.3
+#      final_price = total_price - discount
+#      print(f"Final price after discount: {final_price}") 
+# elif total_price >= 4000:
+#      discount = total_price * 0.4
+#      final_price = total_price - discount
+#      print(f"Final price after discount: {final_price}")
+# else:
+#      print(f"No discount applied. Final price: {total_price}")
+
+
+ # and or not operators
+# age = 50
+# if age >= 18 and age <= 65:
+#     print("You are eligible to work")       
+# else:
+#     print("You are not eligible to work")
+# age = 25
+# if  age <= 18 or age >=  65:
+#     print("You are eligible to work")
+# else:
+#     print("You are not eligible to work")
+
+# age = 15
+# if not (age >= 18 and age <= 65):
+#     print("You are eligible to work")       
+# else:
+#     print("You are not eligible to work")
+#     print(f'what is age' "{age}")
+
+
